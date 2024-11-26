1117ca0cb416ecd838aeae7e0e722a66ec82178f
varick8 <varick.zahir@gmail.com>
2024-11-26 22:50:36 +0700

    update: convert all URL to ENV

--- frontend/utils/apiAccount.ts
+++ frontend/utils/apiAccount.ts
@@ -1,16 +1,17 @@
 import axios from 'axios';
 
-const API_URL = 'https://backend-tugas-akhir-paw-kelompok-13.vercel.app/api/accounts';
+// Use environment variable for the API URL
+const API_URL = process.env.ACCOUNT_API_URL || '';
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
 });
 
 export const apiAccount = {
-  addAccount: async (userId: string, data: { name: string; type: string;
-    initialAmount: number; }) => axiosInstance.post(`/addAccount/${userId}`, data),
-  updateAccount: async (id: string, data: { name?: string; type?: string;
-    initialAmount?: number; }) => axiosInstance.put(`/updateAccount/${id}`, data),
+  addAccount: async (userId: string, data: { name: string; type: string; initialAmount: number }) =>
+    axiosInstance.post(`/addAccount/${userId}`, data),
+  updateAccount: async (id: string, data: { name?: string; type?: string; initialAmount?: number }) =>
+    axiosInstance.put(`/updateAccount/${id}`, data),
   deleteAccount: async (id: string) => axiosInstance.delete(`/deleteAccount/${id}`),
   getAccounts: async (userId: string) => axiosInstance.get(`/getAccount/${userId}`),
 };
